15e68bddfff136b1d336376b7bbc89f8deba9745
Misha <m.mazaixin@yandex.ru>
2024-10-18 18:40:38 +0300

    Add command-line arguments

--- tululu.py
+++ tululu.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 
 from bs4 import BeautifulSoup
@@ -43,22 +44,38 @@ def parse_book_page(response: str) -> dict:
     }
 
 
-for id in range(1, 11):
-    try:
-        url = f'{BOOK_PAGE_URL}{id}/'
+def display_information_about_books(start_id: int, end_id: int) -> None:
+    for id in range(start_id, end_id):
+        try:
+            url = f'{BOOK_PAGE_URL}{id}/'
 
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response=response)
-        
-        title, genres, author, img_url, img_title, comments = parse_book_page(response=response).values()
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response=response)
+            
+            title, genres, author, img_url, img_title, comments = parse_book_page(response=response).values()
 
-        print(f'Заголовок: {title}')
-        print(f'Жанр: {genres}')
-        print(f'Изображение: {img_url}')
-        print(f'Автор: {author}\n')
-        
-        if comments:
-            print(f'Комментарии: {comments}\n')
-    except HTTPError:
-            continue
+            print(f'Заголовок: {title}')
+            print(f'Жанр: {genres}')
+            print(f'Изображение: {img_url}')
+            print(f'Автор: {author}\n')
+            
+            if comments:
+                print(f'Комментарии: {comments}\n')
+        except HTTPError:
+                continue
+
+
+def main():
+    parser = argparse.ArgumentParser(description= 'Retrieves information about the book from the tululu website')
+    
+    parser.add_argument('start_id', type=int, nargs='?', default=1, help='The initial ID of the book')
+    parser.add_argument('end_id', type=int, nargs='?', default=2, help='The final ID of the book')
+    
+    args = parser.parse_args()
+
+    display_information_about_books(start_id=args.start_id, end_id=args.end_id)
+
+
+if __name__ == '__main__':
+    main()
